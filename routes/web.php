85f998084bd5eef5a5971064f4c161b67d1e3f87
Rodrigo Pedra Brum <rodrigo.pedra@gmail.com>
2024-10-13 11:59:25 -0300

    issue

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,50 @@
 <?php
 
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $http = Http::withToken('access_token')
+        ->retry(2, 0, function (Exception $exception, PendingRequest $request) {
+            if (! $exception instanceof RequestException || $exception->response->status() !== 401) {
+                return false;
+            }
+
+            $request->withToken('new_access_token');
+
+            return true;
+        })
+        ->acceptJson()
+        ->asJson()
+        ->timeout(15)
+        ->throw();
+
+    $currentUrl = \url('/endpoint');
+
+    while (!!$currentUrl) {
+        $response = $http->get($currentUrl, [
+            'role' => 'member',
+            'pagelen' => 20,
+        ]);
+        $currentUrl = $response->json('next');
+        dump($currentUrl, $response->json());
+    }
+
+    return true;
+});
+
+Route::get('/endpoint', function (Request $request) {
+    $page = $request->integer('page', 1);
+
+    if ($page > 5) {
+        return ['values' => []];
+    }
+
+    return [
+        'values' => [],
+        'next' => \url('/endpoint') . '?page=' . ($page + 1),
+    ];
 });
